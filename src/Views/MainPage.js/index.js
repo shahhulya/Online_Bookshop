785f341d7826fffd32d1d3385ef56574aae44751
Шахрезада Кожонова <unicorny.monster@gmail.com>
2021-06-23 23:27:38 +0600

    added footer

--- src/Views/MainPage.js/index.js
+++ src/Views/MainPage.js/index.js
@@ -3,13 +3,14 @@ import Card from "../../components/Card";
 import HeroSlider from "../../components/HeroSlider";
 import ItemsSlider from "../../components/ItemsSlider";
 import MainLayout from "../../Layouts/MainLayout";
-
+import Footer from "../../components/Footer/Footer";
 export default function MainPage() {
   return (
     <MainLayout>
       <HeroSlider />
       {/* <ItemsSlider /> */}
       <Card />
+      <Footer />
     </MainLayout>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,6 @@
+import React from "react";
+import classes from "../Footer/footer.module.css";
+
+export default function Footer() {
+  return <div className={classes.footer_wrapper}></div>;
+}
