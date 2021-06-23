c760a83845dab4786689fda8a915560c2d985bd8
Шахрезада Кожонова <unicorny.monster@gmail.com>
2021-06-24 01:44:07 +0600

    footerSocial

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,5 +2,20 @@ import React from "react";
 import classes from "../Footer/footer.module.css";
 
 export default function Footer() {
-  return <div className={classes.footer_wrapper}></div>;
+  return (
+    <div className={classes.footer_wrapper}>
+      <div className={classes.footer_section}>
+        <h3>Разделы</h3>
+        <li>Фильмы</li>
+        <li>Сериалы</li>
+        <li>Трейлеры</li>
+      </div>
+      <div>
+        <h3>О нас</h3>
+        <li>О компании</li>
+        <li>Адреса и контакты магазинов</li>
+        <li>Партнеры</li>
+      </div>
+    </div>
+  );
 }
