21f4d841a0e640a87502fbfa843375547923076a
Шахрезада Кожонова <unicorny.monster@gmail.com>
2021-06-24 18:29:26 +0600

    smth

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import AboutUs from "./components/AboutUs/AboutUs.js";
+import AboutUsPage from "./Views/AboutUsPage/index.js";
 import MainPage from "./Views/MainPage.js";
 
 export default function Routes() {
@@ -7,6 +9,7 @@ export default function Routes() {
     <Router>
       <Switch>
         <Route exact path="/" component={MainPage} />
+        <Route exact path="/AboutUsPage" component={AboutUsPage} />
       </Switch>
     </Router>
   );
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,8 @@
 import React from "react";
 import classes from "../Footer/footer.module.css";
-import AboutUs from "../AboutUs/AboutUs";
+// import AboutUsPage from "../../Views/AboutUsPage";
+
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
@@ -12,7 +14,10 @@ export default function Footer() {
         <li>Обзоры</li>
       </div>
       <div>
-        <h3 onClick={AboutUs}>О нас</h3>
+        <Link to="/AboutUsPage">
+          <h3> О нас</h3>
+        </Link>
+
         <li>О компании</li>
         <li>Адреса и контакты магазинов</li>
         <li>Партнеры</li>
