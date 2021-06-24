d0f97b31a566422b711ebc04af4ced7bbbdc1b3b
Шахрезада Кожонова <unicorny.monster@gmail.com>
2021-06-24 16:32:49 +0600

    added AboutUs

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,17 +1,18 @@
 import React from "react";
 import classes from "../Footer/footer.module.css";
+import AboutUs from "../AboutUs/AboutUs";
 
 export default function Footer() {
   return (
     <div className={classes.footer_wrapper}>
       <div className={classes.footer_section}>
         <h3>Разделы</h3>
-        <li>Фильмы</li>
-        <li>Сериалы</li>
-        <li>Трейлеры</li>
+        <li>Романы</li>
+        <li>Драмы</li>
+        <li>Обзоры</li>
       </div>
       <div>
-        <h3>О нас</h3>
+        <h3 onClick={AboutUs}>О нас</h3>
         <li>О компании</li>
         <li>Адреса и контакты магазинов</li>
         <li>Партнеры</li>
